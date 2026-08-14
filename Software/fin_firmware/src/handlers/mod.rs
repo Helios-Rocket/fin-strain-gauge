c3a47dc3fb477af5a2ee979e9d7d38e638991e0d
cbscott <cbscott6163@gmail.com>
2026-08-14 16:26:16 -0400

    added prototype schem to fin

--- Software/fin_firmware/src/handlers/mod.rs
+++ Software/fin_firmware/src/handlers/mod.rs
@@ -1,3 +1,6 @@
+mod statemachine; 
+use statemachine::Event; 
+
 pub struct StateHandler{
     adc:ADC, 
     winbond_flash:WinbondFlash, 
@@ -6,22 +9,22 @@ pub struct StateHandler{
 
 impl StateHandler{
     pub fn new() -> Self(){}
-    pub fn handle_wait() -> Event{
+    pub fn handle_wait(self) -> Event{
         // Wait for Uart command, return command event
     }
-    pub fn handle_record_data() -> Event{
+    pub fn handle_record_data(self, heartbeat:bool) -> Event{
          //wait for pulse, start recording, iterate page, set flight flag to on,save time, send status 
         //Maybe pulse should be it's own state...
                 // Go into recording state Every x iteration, send heartbeat pwm
                 // Check for command/make stop recording command an interrupt 
     }
-    pub fn handle_stop_recording() -> Event{
+    pub fn handle_stop_recording(self) -> Event{
          // stop recording, change to UART, set flight flag to off, send status, total data, etc. 
     }
-    pub fn handle_erase_flash() -> Event{
+    pub fn handle_erase_flash(self) -> Event{
         // erase flash, send success or failure flag over UART
     } 
-    pub fn handle_error(error) -> Event{
+    pub fn handle_error(self, error:FinError) -> Event{
         // Send error signal over uart
     }
 }
